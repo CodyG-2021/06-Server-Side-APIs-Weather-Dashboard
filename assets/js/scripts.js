188a08eec1819be1bb83a5903d7dc40beb57f7b6
CodyG <gallagher2021@gmail.com>
2021-03-24 15:01:21 -0600

    Able to store the history of the search

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,6 @@
 // 64f2ee2a8261daa4d9f780f5b365f275
+
+$('.search').on("click", function () {
+	var city = $(this).parent('.btnPar').siblings('.textVal').val();
+	localStorage.setItem("city", city);
+});
