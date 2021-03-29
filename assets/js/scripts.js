1edff27b8799fa3fdfecd4ca44167e56288afc82
CodyG <gallagher2021@gmail.com>
2021-03-28 19:30:36 -0600

    Added in the five day forecast with Icons and made button functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,9 @@
 var key = '64f2ee2a8261daa4d9f780f5b365f275';
 var city = "Denver"
 
-var dateTime = moment().format('dddd, MMMM Do YYYY');
-//YYYY-MM-DD HH:MM:SS
-console.log(dateTime);
-
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+// console.log(dateTime);
 
 var cityHist = [];
 //Will save the text value of the search and save it to an array
@@ -15,43 +14,44 @@ $('.search').on("click", function (event) {
 		return;
 	};
 	cityHist.push(city);
-	console.log(cityHist);
 
 	localStorage.setItem('city', JSON.stringify(cityHist));
+	fiveForecastEl.empty();
 	getHistory();
 	getWeatherToday();
 });
 
 //Will create buttons based on search history 
 var contHistEl = $('.cityHist');
-function getHistory () {
+function getHistory() {
 	contHistEl.empty();
 
 	for (let i = 0; i < cityHist.length; i++) {
 
 		var rowEl = $('<row>');
 		var btnEl = $('<button>').text(`${cityHist[i]}`)
-		
+
 		rowEl.addClass('row histBtnRow');
-		btnEl.addClass('btn btn-outline-secondary');
-		btnEl.attr('type','button');
+		btnEl.addClass('btn btn-outline-secondary histBtn');
+		btnEl.attr('type', 'button');
 
 		contHistEl.prepend(rowEl);
 		rowEl.append(btnEl);
-	}
-
-	if (!city){
+	}	if (!city) {
 		return;
-	} 
+	}
+	$('.histBtn').on("click", function (event) {
+		event.preventDefault();
+		city = $(this).text();
+		fiveForecastEl.empty();
+		getWeatherToday();
+	});
 };
 
 //Grab the main 'Today' card body.
 var cardTodayBody = $('.cardBodyToday')
-//Set global vars for lat and long fro the uv index 
-// var cityLon;
-// var cityLat;
 
-function getWeatherToday () {
+function getWeatherToday() {
 	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
 	$(cardTodayBody).empty();
@@ -61,24 +61,26 @@ function getWeatherToday () {
 		method: 'GET',
 	}).then(function (response) {
 		$('.cardTodayCityName').text(response.name);
-		$('.cardTodayDate').text(dateTime);
+		$('.cardTodayDate').text(date);
+		//Icons
+		$('.icons').attr('src', `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
 		// Temperature
 		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		cardTodayBody.append(pEl); 
+		cardTodayBody.append(pEl);
 		//Feels Like
 		var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
-		cardTodayBody.append(pElTemp); 
+		cardTodayBody.append(pElTemp);
 		//Humidity
 		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		cardTodayBody.append(pElHumid); 
+		cardTodayBody.append(pElHumid);
 		//Wind Speed
 		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
 		cardTodayBody.append(pElWind);
 		//Set the lat and long from the searched city
-		var cityLon = response.coord.lon; 
-		console.log(cityLon);
-		var cityLat = response.coord.lat; 
-		console.log(cityLat);
+		var cityLon = response.coord.lon;
+		// console.log(cityLon);
+		var cityLat = response.coord.lat;
+		// console.log(cityLat);
 
 		var getUrlUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
 
@@ -94,23 +96,86 @@ function getWeatherToday () {
 			pElUvi.append(uviSpan);
 			cardTodayBody.append(pElUvi);
 			//set the UV index to match an exposure chart severity based on color 
-			if(uvi >= 0 && uvi <= 2){
-				uviSpan.attr('class','green');
-			}	else if (uvi > 2 && uvi <= 5){
-				uviSpan.attr("class","yellow")
-			}	else if (uvi >5 && uvi <= 7){
-				uviSpan.attr("class","orange")
-			}	else if (uvi >7 && uvi <= 10){
-				uviSpan.attr("class","red")
-			}	else{
-				uviSpan.attr("class","purple")
+			if (uvi >= 0 && uvi <= 2) {
+				uviSpan.attr('class', 'green');
+			} else if (uvi > 2 && uvi <= 5) {
+				uviSpan.attr("class", "yellow")
+			} else if (uvi > 5 && uvi <= 7) {
+				uviSpan.attr("class", "orange")
+			} else if (uvi > 7 && uvi <= 10) {
+				uviSpan.attr("class", "red")
+			} else {
+				uviSpan.attr("class", "purple")
 			}
 		});
+	});
+	getFiveDayForecast();
+};
+
+var fiveForecastEl = $('.fiveForecast');
+
+function getFiveDayForecast() {
+	var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+
+	$.ajax({
+		url: getUrlFiveDay,
+		method: 'GET',
+	}).then(function (response) {
+		var fiveDayArray = response.list;
+		var myWeather = [];
+		$.each(fiveDayArray, function (index, value) {
+			testObj = {
+				date: value.dt_txt.split(' ')[0],
+				time: value.dt_txt.split(' ')[1],
+				temp: value.main.temp,
+				feels_like: value.main.feels_like,
+				icon: value.weather[0].icon,
+				humidity: value.main.humidity
+			}
+
+			if (value.dt_txt.split(' ')[1] === "12:00:00") {
+				myWeather.push(testObj);
+			}
+		})
+
+		for (let i = 0; i < myWeather.length; i++) {
+
+			var divElCard = $('<div>');
+			divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
+			divElCard.attr('style', 'max-width: 200px;');
+			fiveForecastEl.append(divElCard);
+
+			var divElHeader = $('<div>');
+			divElHeader.attr('class', 'card-header')
+			var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
+			divElHeader.text(m);
+			divElCard.append(divElHeader)
+
+			var divElBody = $('<div>');
+			divElBody.attr('class', 'card-body');
+			divElCard.append(divElBody);
+
+			var divElIcon = $('<img>');
+			divElIcon.attr('class', 'icons');
+			divElIcon.attr('src', `http://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+			divElBody.append(divElIcon);
+
+
+			//Temp
+			var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
+			divElBody.append(pElTemp);
+			//Feels Like
+			var pElFeel = $('<p>').text(`Feels Like: ${myWeather[i].feels_like} °F`);
+			divElBody.append(pElFeel);
+			//Humidity
+			var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+			divElBody.append(pElHumid);
+		}
 	});
 };
 
 function initLoad() {
-	
+
 	var cityHistStore = JSON.parse(localStorage.getItem('city'));
 
 	if (cityHistStore !== null) {
@@ -121,24 +186,3 @@ function initLoad() {
 };
 
 initLoad();
-
-
-// var getUrlCurrentUV = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`; 
-// var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial,us&mode=xml&appid=${key}`;
-
-// var getUrlIcons = `https://http://openweathermap.org/img`; 
-
-
-
-
-
-// fetch(getUrlCurrent)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log('Fetch Response');
-//     console.log(data);
-//   });
-
-//imgEl.setAttribute('src',`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
